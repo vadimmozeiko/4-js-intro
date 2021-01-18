65e7e730ec6efff42ba2df04cc6243c7976229ba
Vadim <vadim.mozeiko@gmail.com>
2021-01-18 16:58:50 +0200

    for

---
+++ js/for.js
@@ -0,0 +1,27 @@
+/* 
+for (let i=0 (pradzia); i<5 (pabaiga); i++ (kaip keiciasi) ) {
+    kartotinas kodas
+}
+
+*/
+
+for (let i = 0; i < 5; i++) {
+    console.log(i, 'kodas');
+}
+
+console.log('-----------------');
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i, 'kodas');
+}
+
+console.log('-----------------');
+
+const words = ['suo', 'kate', 'ziurkenas', 'papuga', 'diezas',];
+const wordsLength = words.length
+
+for (let i = 0; i < wordsLength; i++){
+    console.log(i, words[i]);
+
+}
+
+console.log('-----------------');
--- js/js_uzd.js
+++ js/js_uzd.js
@@ -89,201 +89,373 @@ console.log('-------------------------');
 
 // 1. Palyginti tarpusavyje skaiciu kintamuosius
 
-const num1 = 4;
-const num2 = 8;
+// const num1 = 4;
+// const num2 = 8;
 
 
-// a)
+// // a)
 
-if (num1 > num2) {
-    console.log('Pomidoras');
-} else {
-    console.log('Bandykite dar karta');
-};
+// if (num1 > num2) {
+//     console.log('Pomidoras');
+// } else {
+//     console.log('Bandykite dar karta');
+// };
 
-const arDidesnis = num1 > num2 ? 'TAIP, didesnis' : 'NE, mazesnis';
-console.log(arDidesnis); 
+// const arDidesnis = num1 > num2 ? 'TAIP, didesnis' : 'NE, mazesnis';
+// console.log(arDidesnis); 
 
-// b)
-if (num1 < num2) {
-    console.log('Pomidoras');
-} else {
-    console.log('Bandykite dar karta');
-};
+// // b)
+// if (num1 < num2) {
+//     console.log('Pomidoras');
+// } else {
+//     console.log('Bandykite dar karta');
+// };
 
-const arMazesnis = num1 < num2 ? 'TAIP, mazesnis' : 'NE, didesnis';
-console.log(arMazesnis);
+// const arMazesnis = num1 < num2 ? 'TAIP, mazesnis' : 'NE, didesnis';
+// console.log(arMazesnis);
 
-// c)
+// // c)
 
-if (num1 === num2) {
-    console.log('lygus');
-} else {
-    console.log('nelygus');
-};
+// if (num1 === num2) {
+//     console.log('lygus');
+// } else {
+//     console.log('nelygus');
+// };
 
-const arLygus = num1 === num2 ? 'LYGUS' : 'NELYGUS';
-console.log(arLygus);
+// const arLygus = num1 === num2 ? 'LYGUS' : 'NELYGUS';
+// console.log(arLygus);
 
 
-// d)
+// // d)
 
-if (num1 !== num2) {
-    console.log('nelygus');
-} else {
-    console.log('lygus');
-};
+// if (num1 !== num2) {
+//     console.log('nelygus');
+// } else {
+//     console.log('lygus');
+// };
 
-// e)
+// // e)
 
-if (num1 >= num2) {
-    console.log('taip');
-} else {
-    console.log('ne');
-};
+// if (num1 >= num2) {
+//     console.log('taip');
+// } else {
+//     console.log('ne');
+// };
 
-// f)
+// // f)
 
-if (num1 <= num2) {
-    console.log('taip');
-} else {
-    console.log('ne');
-};
+// if (num1 <= num2) {
+//     console.log('taip');
+// } else {
+//     console.log('ne');
+// };
 
-// 2. Isvesti teksto kintamuju ilgius
+// // 2. Isvesti teksto kintamuju ilgius
 
-const text = 'Kazkoks tekstas'
-const text1 = 'Dar kazkoks tekstas'
+// const text = 'Kazkoks tekstas'
+// const text1 = 'Dar kazkoks tekstas'
 
-console.log(text.length);
-console.log(text1.length);
+// console.log(text.length);
+// console.log(text1.length);
 
-const textLength = text.length;
-const textLength1 = text1.length;
+// const textLength = text.length;
+// const textLength1 = text1.length;
 
-// 3. Tarpusavyje palyginti teksto tipo kintamuju ilgius
+// // 3. Tarpusavyje palyginti teksto tipo kintamuju ilgius
 
-// a)
+// // a)
 
-if (textLength > textLength1) {
-    console.log('Taip, ilgesnis');
-} else {
-    console.log('Ne, neilgesnis');
-};
+// if (textLength > textLength1) {
+//     console.log('Taip, ilgesnis');
+// } else {
+//     console.log('Ne, neilgesnis');
+// };
 
-// b)
+// // b)
 
-if (textLength < textLength1) {
-    console.log('Taip, ilgesnis');
-} else {
-    console.log('Ne, neilgesnis');
-};
+// if (textLength < textLength1) {
+//     console.log('Taip, ilgesnis');
+// } else {
+//     console.log('Ne, neilgesnis');
+// };
 
-// c)
+// // c)
 
-if (textLength === textLength1) {
-    console.log('Taip, lygus');
-} else {
-    console.log('Ne, nelygus');
-};
+// if (textLength === textLength1) {
+//     console.log('Taip, lygus');
+// } else {
+//     console.log('Ne, nelygus');
+// };
 
-// d) 
+// // d) 
 
-if (textLength !== textLength1) {
-    console.log('Ne, nelygus');
-} else {
-    console.log('Taip, lygus');
-};
+// if (textLength !== textLength1) {
+//     console.log('Taip, nelygus');
+// } else {
+//     console.log('Ne, lygus');
+// };
 
-// e)
+// // e)
 
-if (textLength >= textLength1) {
-    console.log('Taip');
-} else {
-    console.log('Ne');
-};
+// if (textLength >= textLength1) {
+//     console.log('Taip');
+// } else {
+//     console.log('Ne');
+// };
 
-// f)
+// // f)
 
-if (textLength <= textLength1) {
-    console.log('Taip');
-} else {
-    console.log('Ne');
-};
+// if (textLength <= textLength1) {
+//     console.log('Taip');
+// } else {
+//     console.log('Ne');
+// };
 
 // ----------------------Usign ternary, if and switch---------------
 
-const noFuel = true; // true,false
-const fuelType = ''; // petrol, diesel
+// const noFuel = true; // true,false
+// const fuelType = ''; // petrol, diesel
 
-const needFuel = noFuel === true ? 
-                fuelType === 'petrol' ? 
-                    'Fill up with petrol' : 
-                    fuelType === 'diesel' ?
-                        'Fill up with diesel'
-                        : 'Check fuel type' :
-                'Do not need fuel';
-console.log(needFuel);
+// const needFuel = noFuel === true ? 
+//                 fuelType === 'petrol' ? 
+//                     'Fill up with petrol' : 
+//                     fuelType === 'diesel' ?
+//                         'Fill up with diesel'
+//                         : 'Check fuel type' :
+//                 'Do not need fuel';
+// console.log(needFuel);
 
-console.log('----------IF----------------');
+// console.log('----------IF----------------');
 
-if (noFuel === true) {
-    if (fuelType === "petrol"){
-        console.log('Fill up with PETROL');
-    } else if (fuelType === 'diesel') {
-        console.log('Fill up with DIESEL');
-    } else {
-        console.log('Check fuel TYPE');
-    }
-} else {
-    console.log('Do no need FUEL');
-};
+// if (noFuel === true) {
+//     if (fuelType === "petrol"){
+//         console.log('Fill up with PETROL');
+//     } else if (fuelType === 'diesel') {
+//         console.log('Fill up with DIESEL');
+//     } else {
+//         console.log('Check fuel TYPE');
+//     }
+// } else {
+//     console.log('Do no need FUEL');
+// };
 
-console.log('----------SWITCH----------------');
+// console.log('----------SWITCH----------------');
 
-switch (noFuel) {
-    case true : 
-        switch (fuelType){
-            case 'petrol': 
-                console.log('!PETROL');
-                break;
-            case 'diesel': 
-                console.log('!DIESEL');
-                break;
-            default :  
-            console.log('!CHECK');
-            break;     
-    } 
-        break;
-    default : 
-        console.log('---DONT NEED FUEL---');
-        break; 
-};
+// switch (noFuel) {
+//     case true : 
+//         switch (fuelType){
+//             case 'petrol': 
+//                 console.log('!PETROL');
+//                 break;
+//             case 'diesel': 
+//                 console.log('!DIESEL');
+//                 break;
+//             default :  
+//             console.log('!CHECK');
+//             break;     
+//     } 
+//         break;
+//     default : 
+//         console.log('---DONT NEED FUEL---');
+//         break; 
+// };
 
 
-console.log('----------SWITCH with interaction----------------');
+// console.log('----------SWITCH with interaction----------------');
 
-let myFuelLevel = prompt('Whats your fuel level (low or high?)');
+// let myFuelLevel = prompt('Whats your fuel level (low or high?)');
 
-switch (myFuelLevel) {
-    case 'low' : 
-        let myFuelType = prompt('Whats your fuel type (petrol or diesel?)');
-        switch (myFuelType) {
-            case 'petrol': 
-                alert('!FILL UP PETROL');
-                break;
-            case 'diesel': 
-                alert('!FILL UP DIESEL');
-                break;
-            default :  
-            alert('!I DONT KNOW THIS ONE!');
-            break;     
-    } 
-        break;
-    case 'high' :
-        alert('---DONT NEED FUEL---');
-        break; 
-    default : 
-        alert('---MAKE YOUR MIND---');
-        break; 
-};
+// switch (myFuelLevel) {
+//     case 'low' : 
+//         let myFuelType = prompt('Whats your fuel type (petrol or diesel?)');
+//         switch (myFuelType) {
+//             case 'petrol': 
+//                 alert('!FILL UP PETROL');
+//                 break;
+//             case 'diesel': 
+//                 alert('!FILL UP DIESEL');
+//                 break;
+//             default :  
+//             alert('!I DONT KNOW THIS ONE!');
+//             break;     
+//     } 
+//         break;
+//     case 'high' :
+//         alert('---DONT NEED FUEL---');
+//         break; 
+//     default : 
+//         alert('---MAKE YOUR MIND---');
+//         break; 
+// };
+
+
+// 4) Isvesti saraso tipo kintamuju ilgius
+
+
+// const array = [4, 8, 10, 'Ne', 'Taip'];
+
+// console.log(array.length);
+
+// // 5) Tarpusavyje palyginti saraso tipo kintamuju ilgius
+
+
+// const array1 = [11, 5, 6, 9, 'K', 'L'];
+// const array2 = ['Z', 'B', 7, 4, 6, 2, 1, 1];
+
+// const arrayLength1 = array1.length;
+// const arrayLength2 = array2.length;
+
+// if (arrayLength1 > arrayLength2) {
+//     console.log('Taip');
+// } else {
+//     console.log('Ne');
+// };
+
+// if (arrayLength1 < arrayLength2) {
+//     console.log('Taip');
+// } else {
+//     console.log('Ne');
+// };
+
+// if (arrayLength1 === arrayLength2) {
+//     console.log('Taip, lygus');
+// } else {
+//     console.log('Ne, nelygus');
+// };
+
+// if (arrayLength1 !== arrayLength2) {
+//     console.log('Taip, nelygus');
+// } else {
+//     console.log('Ne, lygus');
+// };
+
+
+// if (arrayLength1 <= arrayLength2) {
+//     console.log('Taip');
+// } else {
+//     console.log('Ne');
+// };
+
+
+// if (arrayLength1 >= arrayLength2) {
+//     console.log('Taip');
+// } else {
+//     console.log('Ne');
+// };
+
+
+// const pazymiai = [10, 2, 8, 4, 6, 4, 8, 2, 10,];
+
+// let vidurkis = 0;
+
+
+// for (let i = 0; i < pazymiai.length; i++) {
+//    vidurkis += pazymiai[i] / pazymiai.length;
+   
+// }
+
+// console.log(vidurkis);
+
+// Ciklo for panaudojimas -----------------------
+
+// 1 Suskaiciuoti susumavus skaicius intervale tarp (imtinai)
+
+// let sumaA = 0;
+
+// for (let i = 0; i < 0; i++) {
+//     sumaA += i;
+// }
+
+// console.log(sumaA);
+
+// let sumaB = 0;
+
+// for (let i = 0; i <= 4; i++) {
+//     sumaB += i;
+// }
+
+// console.log(sumaB);
+
+// let sumaC = 0;
+
+// for (let i = 0; i <= 100; i++) {
+//     sumaC += i;
+// }
+
+// console.log(sumaC);
+
+
+// let sumaD = 0;
+
+// for (let i = 574; i <= 815; i++) {
+//     sumaD += i;
+// }
+
+// console.log(sumaD);
+
+// let sumaE = 0;
+
+// for (let i = -50; i <= 50; i++) {
+//     sumaE += i;
+// }
+
+// console.log(sumaE);
+
+// let sumaF = 0;
+
+// for (let i = -70; i <= 30; i++) {
+//     sumaF += i;
+// }
+
+// console.log(sumaF);
+
+// 2 Perrasyti teksto tipo kintamuju reikmes is kito galo
+
+// a)
+
+// const tekstasX = 'abcdef';
+// let tekstasY = '';
+
+// // paima raides is eiles ir prideda kiekviena raide i saraso pradzia
+// for (let i = 0; i < tekstasX.length; i++) {
+//      const raide = tekstasX[i]; /skaiciuoja is eiles
+//      tekstasY = raide + tekstasY;
+// };
+
+// console.log(tekstasX, '->', tekstasY);
+
+
+// // paima raides is galo ir deda is eiles
+// for (let i = 0; i < tekstasX.length; i++) {
+//     const raide = tekstasX[tekstasX.length - i - 1]; // skaciuoja nuo galo
+//     tekstasY += raide;
+// };
+
+// console.log(tekstasX, '->', tekstasY);
+
+// paima raides is galo ir deda is eiles
+// for (let i = 1; i <= tekstasX.length; i++) {
+//     const raide = tekstasX[tekstasX.length - i];
+//     tekstasY += raide;
+// };
+
+// console.log(tekstasX, '->', tekstasY);
+
+// 3 Suskaičiuoti, kiek nurodytame intervale yra skaičių, kurie dalijasi be liekanos iš 3, 5 ir 7 atskirai
+
+// a)
+
+const from = 0;
+const till = 11;
+const divider = 5;
+
+let count = 0;
+
+for (let i = from; i <= till; i++) {
+    if (i % divider === 0) {
+        count++
+    }   
+}
+const ats = `Skaičių intervale tarp ${from} ir ${till}, besidalijančių be liekanos iš ${divider} yra ${count} vienetai.`
+
+console.log(ats);
